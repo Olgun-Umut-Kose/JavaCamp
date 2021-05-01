5aa49fcd0db850f595d16b85c8ab2a1eb03a48d1
BGOuk <ummut22005@gmail.com>
2021-05-01 23:05:19 +0300

    2021 1 mayıs Workshop 1

--- interfaces/src/com/bgouk/Main.java
+++ interfaces/src/com/bgouk/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CustomerManager customerManager = new CustomerManager(new EmailLogger());
+        CustomerManager customerManager = new CustomerManager(new EmailLogger(),new SmsLogger());
         Customer customer = new Customer(1,"Engin","Demiroğ");
         customerManager.add(customer);
     }
