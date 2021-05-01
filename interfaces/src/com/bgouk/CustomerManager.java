ef038acaf14171fb722147e534befc5c6922f180
BGOuk <ummut22005@gmail.com>
2021-05-01 23:04:53 +0300

    2021 1 mayıs Workshop 1

--- interfaces/src/com/bgouk/CustomerManager.java
+++ interfaces/src/com/bgouk/CustomerManager.java
@@ -2,18 +2,24 @@ package com.bgouk;
 
 public class CustomerManager {
 
-    private final Logger logger;
+    private final Logger[] loggers;
 
-    public CustomerManager(Logger logger){
-        this.logger = logger;
+    public CustomerManager(Logger... logger){
+        this.loggers = logger;
     }
 
     public void add(Customer customer){
         System.out.println("Müşteri eklendi " + customer.getFirstName());
-        logger.log(customer.getFirstName() + " isimli müşteri eklendi");
+        for (Logger logger:loggers) {
+            logger.log(customer.getFirstName() + " isimli müşteri eklendi");
+        }
+
     }
     public void delete(Customer customer){
         System.out.println("Müşteri silindi " + customer.getFirstName());
-        logger.log(customer.getFirstName() + " isimli müşteri silindi");
+        for (Logger logger:loggers) {
+            logger.log(customer.getFirstName() + " isimli müşteri silindi");
+        }
+
     }
 }
